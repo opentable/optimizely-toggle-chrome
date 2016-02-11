25152353b5b06b207809894c0c9430cec80b02ba
Federico Maffei <federico.maffei@gmail.com>
2016-02-11 16:00:26 +0000

    safer way of creating and injecting html

--- alert.js
+++ alert.js
@@ -1,3 +1,16 @@
-$.get(chrome.extension.getURL("alert.html"), function(data) {
-    document.body.insertAdjacentHTML('afterbegin',  data);
-});
+var container = document.createElement("div");
+var style = document.createElement("style");
+var alertContainer = document.createElement("div");
+var paragraph = document.createElement("p");
+paragraph.className += "optimizely-alert-text";
+alertContainer.id = "optimizely-alert";
+
+style.textContent = "#optimizely-alert{position:fixed;bottom:0;right:0;width:200px;background-color:#fff;z-index:9999;border:2px solid #D62430;-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box}.optimizely-alert-text{font-family:arial,sans-serif!important;font-size:13px;font-weight:400;padding:5px;margin:0;line-height:1;-moz-osx-font-smoothing:auto}";
+
+paragraph.textContent = "Optimizely disabled by toggle.";
+
+alertContainer.appendChild(paragraph);
+container.appendChild(style);
+container.appendChild(alertContainer);
+
+document.body.appendChild(container);
